582fbc19217630e4163f77cf0fffc13872e8ab9b
squarepusher03 <gbh33@drexel.com>
2025-12-04 18:02:35 -0500

    This is not finished, just a checkpoint to go between devices

--- download.py
+++ download.py
@@ -2,7 +2,7 @@
 
 from pyspedas import mms, download
 
-rnge = ['2020-09-02/06:30', '2020-09-02/07:00']
+rnge = ['2019-08-16/09:31', '2019-08-16/09:33']
 
 mms.scm(trange=rnge, time_clip=True, datatype='schb', data_rate='brst', latest_version=True, no_update=False)
 mms.edp(trange=rnge, time_clip=True, datatype='dce', data_rate='brst', latest_version=True, no_update=False)
--- ffttest.py
+++ ffttest.py
@@ -5,15 +5,18 @@ import os
 from time import strftime
 
 import numpy as np
+from matplotlib.patches import Patch
+from matplotlib.text import Text
 from scipy import fft
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from matplotlib.ticker import FuncFormatter
+from matplotlib.ticker import FuncFormatter, NullFormatter, NullLocator, MultipleLocator, LogLocator, FixedLocator
 from matplotlib import rcParams
 from scipy.signal.windows import hann
 from matplotlib.widgets import Slider
+from scipy.stats import binned_statistic_2d
 
 SCHB_FS = 16_384
 
@@ -79,11 +82,19 @@ pkl_path_e = '.cache/edp/dce/20200902062933.pkl'
 
 sfc = 'mms_data/mms1/scm/brst/l2/schb/2020/09/02/mms1_scm_brst_l2_schb_20200902062933_v2.2.0.cdf'  # scm source
 pkl_path_s = '.cache/scm/schb/20200902062933.pkl'
+
+paper = 'mms_data/mms1/scm/brst/l2/schb/2019/08/16/mms1_scm_brst_l2_schb_20190816093103_v2.2.0.cdf'
+pkl_path_p = '.cache/scm/schb/20190816093145.pkl'
 if __name__ == "__main__":
     rcParams['path.simplify'] = True
     rcParams['path.simplify_threshold'] = 0.2
-    rcParams['savefig.format'] = 'svg'
+    #rcParams['savefig.format'] = 'svg'
     mpl.use('Qt5Agg')
+
+    lpf_lim = 1000
+    hpf_lim = 10
+    num_bins = 24
+    bin_factor = (lpf_lim) ** (1 / num_bins)
 
     # all scm caches for graphing all files
     # sfc = glob.glob('mms_data/mms1/scm/brst/l2/schb/2020/09/02/*')
@@ -92,36 +103,37 @@ if __name__ == "__main__":
     sig_key = 'Bz'
     units = r'\frac{\text{nT}^2}{\text{Hz}}' if sig_key[0] == 'B' else r'\frac{\text{mV}^2}{\text{m}^2 \cdot \text{Hz}}'
 
-    cache_path = sfc
-    pkl_path = pkl_path_s
+    cache_path = paper
+    pkl_path = pkl_path_p
 
     df = load_pickle_safe(pkl_path, cache_path, sig_key)
     df = df.dropna().reset_index(drop=True)
 
-    start = datetime(2020, 9, 2, 6, 30, 54)
-    end = datetime(2020, 9, 2, 6, 31, 6)
-    print(df)
+    datefmt = '%m/%d/%Y-%H:%M:%S'
+    start = datetime.strptime('08/16/2019-09:31:45', datefmt)
+    end = datetime.strptime('08/16/2019-09:32:15', datefmt)
 
     # Create two axes: top for Ez vs time, bottom for FFT/PSD
     fig, ax = plt.subplots(
-        1, 1, figsize=(8, 6), sharex=False, constrained_layout=True
+        1, 1, figsize=(10, 8), sharex=False, constrained_layout=True
     )
 
-
-
-    st = datetime(2020, 9, 2, 6, 30, 56)
-    ts = st + timedelta(seconds=1.25)
+    st = start
+    ts = end
     times = [np.arange((st - start).total_seconds(), (ts - start).total_seconds(), 0.05)]
 
     fdf = pd.DataFrame(columns=['time', 'frequency', 'power'])
 
-    for i in np.arange(0, 0.75, 0.05):
+    for i in np.arange(0, (ts - st).total_seconds(), 0.05):
         center = st + timedelta(seconds=(float(i)))
         fft_start = center - timedelta(seconds=(float(0.5)))
         fft_end = center + timedelta(seconds=(float(0.5)))
 
         tdf = df[(df['time'] >= fft_start) & (df['time'] <= fft_end)].reset_index(drop=True)
-        signal = tdf[sig_key]
+        if tdf.empty:
+            continue
+        #signal = tdf[sig_key]
+        signal = np.sqrt(tdf[sig_key[0] + 'z'] ** 2 + tdf[sig_key[0] + 'y'] ** 2 + tdf[sig_key[0] + 'x'] ** 2)
         signal = signal - signal.mean()
 
         n = len(signal)
@@ -146,8 +158,8 @@ if __name__ == "__main__":
             if signal_psd.size > 1:
                 signal_psd[1:] *= 2
 
-        # Frequency mask — keep only (0, 2000] Hz and apply to BOTH arrays
-        mask = (signal_fft_freq > 0) & (signal_fft_freq <= 2000)
+        mask = (signal_fft_freq > 0) & (signal_fft_freq <= lpf_lim)
+
         f_sel = signal_fft_freq[mask]
         P_sel = signal_psd[mask]
         fdf = pd.concat([fdf, pd.DataFrame({
@@ -155,18 +167,56 @@ if __name__ == "__main__":
             'frequency': f_sel,
             'power': P_sel})], ignore_index=True)
 
+    i = lpf_lim
+    bins = list()
+    while i > 2: # TODO: count how many bins are in the graph and update this
+        bins.append(int(i))
+        i = i / bin_factor
+    bins.append(1)
+    bins.reverse()
+
+    def channelize(x):
+        if x == lpf_lim:
+            return len(bins) - 1
+        for i in range(1, len(bins)):
+            if bins[i - 1] <= x < bins[i]:
+                return i
+        return None
+
+    vfunc = np.vectorize(channelize)
+    fdf['channel'] = vfunc(fdf['frequency'])
+
     # Bottom: PSD (FFT) plot
     time_bins = np.arange(fdf['time'].min(), fdf['time'].max() + 0.06, 0.05)
-    freq_bins = np.linspace(0, 2000, 200)
-    ax.xaxis.set_major_locator(mpl.ticker.MultipleLocator(0.05, 2))
+    ax.yaxis.set_major_locator(NullLocator())
+    ax.xaxis.set_major_locator(MultipleLocator(1, 1))
 
-    ax.hist2d(fdf['time'], fdf['frequency'], weights=fdf['power'], bins=[time_bins, freq_bins],
-              cmap='viridis', norm=mpl.colors.LogNorm(), cmin=10e-12, cmax=10e-6)
-    ax.set_ylim(0, 2000)
+    stat, xe, ye, _ = binned_statistic_2d(fdf['time'], fdf['channel'], fdf['power'],
+                                          statistic='median', bins=[time_bins, list(range(1, len(bins) + 1))])
 
-    ax.set_ylabel(r'Frequency (Hz)')
-    ax.set_title(fr'${sig_key}$ Frequency vs. Time vs. ${sig_key}$ Power ${units}$')
+    mappable = plt.pcolormesh(xe, ye, stat.T,
+                   norm=mpl.colors.LogNorm(), cmap='jet')
+    cbar = plt.colorbar(mappable=mappable, ax=ax)
+    cbar.formatter = FuncFormatter(lambda x, pos: f'{int(np.log10(x))}')
+
+    #ax.set_ylim(bottom=7)
+    ax.set_xlim(right=30.5)
+
+    lg_lbls = [f'Channel {i}: {bins[i-1]} Hz - {bins[i]} Hz' for i in range(1, len(bins))]
+    proxies = [Patch(color='none') for _ in range(len(lg_lbls))]
+
+    ax.legend(proxies, lg_lbls, handlelength=0, handletextpad=0)
+
+    #ax.hist2d(fdf['time'], fdf['frequency'], weights=fdf['power'], bins=[time_bins, freq_bins],
+    #         cmap='viridis', norm=mpl.colors.LogNorm(), cmin=10e-12, cmax=10e-6)
+
+    ax.set_ylabel('Frequency (Hz)')
+    ax.set_title(fr'$|{sig_key[0]}|$ Frequency vs. Time vs. $|{sig_key[0]}|$ Power ${units}$')
     ax.set_xlabel('Time (s)')
 
-    plt.tight_layout()
-    plt.show()
+#    ytx = ax.get_yticks()
+#    ytl = [str(int(i)) for i in ytx[:-2]] + ['']
+    #ax.set_yticklabels(ytl)
+
+    #plt.show()
+    plt.savefig('./out/scm/fft/20bins.png')
